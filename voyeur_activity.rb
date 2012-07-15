bc893a7aa66caba9d25e8ffa24d573407c7dde34
Jeremy <na>
2012-07-15 14:29:57 -0700

    memory leaks with bitmaps and face detector fixed.

--- voyeur_activity.rb
+++ voyeur_activity.rb
@@ -16,6 +16,7 @@
 require 'ruboto/activity'
 require 'ruboto/widget'
 require 'ruboto/util/stack'
+require 'ruboto/util/toast'
 require 'ruboto/generate'
 
 ruboto_import_widgets :TextView, :LinearLayout, :Button
@@ -34,7 +35,6 @@ java_import 'java.io.FileNotFoundException'
 java_import 'android.net.Uri'
 java_import 'android.graphics.BitmapFactory'
 java_import 'android.graphics.Bitmap'
-#java_import 'android.graphics.Bitmap.CompressFormat'
 java_import 'android.media.FaceDetector'
 java_import 'android.graphics.Canvas'
 java_import 'android.graphics.Color'
@@ -45,6 +45,8 @@ java_import 'android.view.SurfaceHolder'
 java_import 'android.view.SurfaceView'
 java_import 'android.view.Window'
 
+java_import 'android.os.Debug'
+
 ruboto_import_widget :SurfaceView, "android.view"
 
 module SurfaceDraw
@@ -54,106 +56,107 @@ module SurfaceDraw
     end_draw
   end
 
-  def setup_draw(holder)
+  def setup_draw(holder=nil)
     Log.i("VOYEUR", "setup_draw called with holder: #{holder}")
-    
-    @surface = holder.getSurface
+    @holder = holder if holder
+    @surface = @holder.getSurface
     Log.i("VOYEUR", "surface was #{@surface.inspect}")
-
-    @surface_frame = holder.getSurfaceFrame
-
-    @mBitmap ||= BitmapFactory.decodeFile("/sdcard/123.jpg")
-
+    @surface_frame = @holder.getSurfaceFrame
+    save_file = "/sdcard/123save.jpg"
+    if File.exists?(save_file)
+      use_this_file = save_file
+    else
+      use_this_file = "/sdcard/123.jpg"
+    end
+    @mBitmap ||= BitmapFactory.decodeFile(use_this_file)
     @viewWidth = @surface_frame.width
     @viewHeight = @surface_frame.height
-
     @drawing_cache_bitmap ||= Bitmap.createBitmap(@viewWidth, @viewHeight, Bitmap::Config::ARGB_8888)
     @alt_canvas ||= Canvas.new(@drawing_cache_bitmap)
-
-    holder.synchronized do 
-      @canvas = holder.lockCanvas(nil)
+    @holder.synchronized do 
+      @canvas = @holder.lockCanvas(nil)
     end
-    Log.i("VOYEUR", "lockCanvas returned #{@canvas.inspect}")
+  end
+
+  def init_drawing(holder=nil)
+    @holder = holder if holder
+    setup_draw(@holder)
+    #detect_faces
+    @alt_canvas.drawBitmap(@mBitmap, nil, Rect.new(0,0, @viewWidth, @viewHeight), @tmpPaint)
+    flush_to_canvas
+    end_draw
+  end
+
+  def undo_all
+    init_drawing
+  end
+
+  def flush_to_canvas
+    @canvas.drawBitmap(@drawing_cache_bitmap, nil, Rect.new(0,0, @viewWidth, @viewHeight), @tmpPaint) 
   end
 
   def end_draw
     @surface.unlockCanvasAndPost(@canvas)
   end
 
-  def draw_over_face
-    @already_run = true
-    @num_faces ||= 1
-    @getAllFaces ||= FaceDetector::Face[@num_faces].new
-    @eyesMidPts = Array.new
-    @eyesDistance ||= Array.new
-    @picWidth ||= @mBitmap.getWidth.to_f
-    @picHeight ||= @mBitmap.getHeight.to_f
-
-    @viewWidth = @surface_frame.width
-    @viewHeight = @surface_frame.height
-
-    @xRatio = @viewWidth / @picWidth
-    @yRatio = @viewHeight / @picHeight
-    @pInnerBullsEye ||= Paint.new(Paint::ANTI_ALIAS_FLAG)
-    @pOuterBullsEye ||= Paint.new(Paint::ANTI_ALIAS_FLAG)
-    @pInnerBullsEye.setStyle(Paint::Style::FILL)
-    @pInnerBullsEye.setColor(Color::GRAY)
-    @pOuterBullsEye.setStyle(Paint::Style::STROKE)
-    @pOuterBullsEye.setColor(Color::GRAY) 
-
-    if @arrayFaces
-      Log.i("VOYEUR", "already detected the faces, skipping")
-    else
-      @arrayFaces = FaceDetector.new( @picWidth, @picHeight, @num_faces )
-      @arrayFaces.findFaces(@mBitmap, @getAllFaces)
-      @eyesMP = PointF.new
-      @the_eyes = Array.new
-      @getAllFaces.each do |face|
-        Log.i("VOYEUR", "begin face detection")
-        if face != nil
-          Log.i("VOYEUR", "found a face!")
-          face.getMidPoint(@eyesMP)
-          @the_eyes << {:midpoint => @eyesMP, :eye_distance => face.eyesDistance}
-        end
+  def draw_over_faces
+    setup_draw
+    num_faces = 1
+    getAllFaces = FaceDetector::Face[num_faces].new
+    eyesMidPts = Array.new
+    eyesDistance = Array.new
+    picWidth = @mBitmap.getWidth.to_f
+    picHeight = @mBitmap.getHeight.to_f
+    viewWidth = @surface_frame.width
+    viewHeight = @surface_frame.height
+    xRatio = viewWidth / picWidth
+    yRatio = viewHeight / picHeight
+    arrayFaces = FaceDetector.new( picWidth, picHeight, num_faces )
+    arrayFaces.findFaces(@mBitmap, getAllFaces)
+    eyesMP = PointF.new
+    the_eyes = Array.new
+    getAllFaces.each do |face|
+      Log.i("VOYEUR", "begin face detection")
+      if face != nil
+        Log.i("VOYEUR", "found a face!")
+        @found_faces = true
+        face.getMidPoint(eyesMP)
+        the_eyes << {:midpoint => eyesMP, :eye_distance => face.eyesDistance}
       end
     end
-    @tmpPaint = Paint.new(Paint::ANTI_ALIAS_FLAG)
-    @tmpPaint.setStyle(Paint::Style::STROKE)
-    @tmpPaint.setTextAlign(Paint::Align::CENTER)
-    @alt_canvas.drawBitmap(@mBitmap, nil, Rect.new(0,0, @viewWidth, @viewHeight), @tmpPaint)
-    @the_eyes.each do |face|
-      #canvas.drawCircle(face[:midpoint].x * xRatio, face[:midpoint].y * yRatio, face[:eye_distance].to_f, pInnerBullsEye);
-      #canvas.drawCircle(face[:midpoint].x * xRatio, face[:midpoint].y * yRatio, face[:eye_distance].to_f / 2, pOuterBullsEye);
-      #canvas.drawCircle(face[:midpoint].x * xRatio, face[:midpoint].y * yRatio, face[:eye_distance].to_f / 3, pOuterBullsEye);
-      @alt_canvas.drawCircle(face[:midpoint].x * @xRatio, face[:midpoint].y * @yRatio, face[:eye_distance].to_f / 4, @pInnerBullsEye);
-      #canvas.drawCircle(face[:midpoint].x * xRatio, face[:midpoint].y * yRatio, face[:eye_distance].to_f * 1.2, pOuterBullsEye);
-      #canvas.drawCircle(face[:midpoint].x * xRatio, face[:midpoint].y * yRatio, face[:eye_distance].to_f * 1.3, pOuterBullsEye);
-      #canvas.drawCircle(face[:midpoint].x * xRatio, face[:midpoint].y * yRatio, face[:eye_distance].to_f * 1.4, pOuterBullsEye);
-      #canvas.drawCircle(face[:midpoint].x * xRatio, face[:midpoint].y * yRatio, face[:eye_distance].to_f * 1.5, pOuterBullsEye);
-    end
+    tmpPaint = Paint.new(Paint::ANTI_ALIAS_FLAG)
+    tmpPaint.setStyle(Paint::Style::STROKE)
+    tmpPaint.setTextAlign(Paint::Align::CENTER)
+    @alt_canvas.drawBitmap(@mBitmap, nil, Rect.new(0,0, viewWidth, viewHeight), tmpPaint)
 
+    color = Paint.new(Paint::ANTI_ALIAS_FLAG)
+    color.setStyle(Paint::Style::FILL)
+    color.setColor(Color::GRAY)
+    the_eyes.each do |face|
+      @alt_canvas.drawCircle(face[:midpoint].x * xRatio, face[:midpoint].y * yRatio, face[:eye_distance].to_f * xRatio, color);
+    end
+    flush_to_canvas
+    end_draw
+    @found_faces
+  end
+
+  def recycle
+    @mBitmap.recycle
+    @drawing_cache_bitmap.recycle
   end
 end
 
 # The callbacks for the Edit View
 class RubotoSurfaceHolderCallback
   include SurfaceDraw
-  attr_accessor :drawing_cache_bitmap, :alt_canvas
-
-  def mainloop(holder)
-    setup_draw(holder)
-    draw_over_face
-    end_draw
-    true
-  end
-
   def set_touch_listener(tl)
     @touch_listener = tl
   end
 
   def surfaceCreated(holder)
     Log.i("VOYEUR", "surfaceCreate called")
-    mainloop(holder)
+    # use the touch_listener to initialize the drawing surface
+    @touch_listener.init_drawing(holder)
     true
   end
 
@@ -163,12 +166,16 @@ class RubotoSurfaceHolderCallback
 
   def surfaceDestroyed(holder)
     Log.i("VOYEUR", "surfaceDestroyed called")
+    @touch_listener.recycle
+    #Debug.stopAllocCounting
+    #Debug.stopMethodTracing
   end
 end
 
 class OnTouchListener
   include SurfaceDraw
   attr_accessor :drawing_cache_bitmap
+
   def onTouch(view, event)
     Log.i("VOYEUR", "onTouch called")
     x = event.getX
@@ -178,12 +185,11 @@ class OnTouchListener
     color.setStyle(Paint::Style::FILL)
     color.setColor(Color::GRAY)
     setup_draw(view.getHolder)
-    draw_over_face unless @already_run
     @alt_canvas.drawCircle(point[0], point[1], 7.0, color)
     @tmpPaint ||= Paint.new(Paint::ANTI_ALIAS_FLAG)
     @tmpPaint.setStyle(Paint::Style::STROKE)
     @tmpPaint.setTextAlign(Paint::Align::CENTER)
-    @canvas.drawBitmap(@drawing_cache_bitmap, nil, Rect.new(0,0, view.getWidth, view.getHeight), @tmpPaint) 
+    flush_to_canvas
     end_draw
     Log.i("VOYEUR", "onTouch draw complete")
     true
@@ -199,8 +205,10 @@ class RubotoActivity
         setContentView(linear_layout(:orientation => :vertical) do
           linear_layout do
             #text_view :text => "hellotext"
-            button :text => "Undo All Changes", :on_click_listener => @handle_click_back
+            button :text => "Menu", :on_click_listener => @handle_click_menu
+            button :text => "Undo All", :on_click_listener => @handle_click_undo
             button :text => "Save", :on_click_listener => @handle_click_save
+            button :text => "Face", :on_click_listener => @handle_click_face
           end
           @sv = surface_view
           @callback ||= RubotoSurfaceHolderCallback.new
@@ -211,16 +219,26 @@ class RubotoActivity
         end)
       end
       @handle_click_save = proc do |view|
-        Log.i("VOYEUR", "GONNA SAVE THAT")
         savefile = JFile.new("/sdcard/123save.jpg")
         outStream = FileOutputStream.new(savefile)
         @touch_listener.drawing_cache_bitmap.compress(Bitmap::CompressFormat::JPEG, 100, outStream);
         outStream.flush
         outStream.close
+        toast "saved."
       end
-      @handle_click_back = proc do |view|
-        Log.i("VOYEUR", "GONNA GO BACK")
+      @handle_click_menu = proc do |view|
         RubotoActivity.launch(self)
+      end
+      @handle_click_face = proc do |view|
+        unless @touch_listener.draw_over_faces
+          toast "no face detected."
+        end
+      end
+      @handle_click_undo = proc do |view|
+        del_this = JFile.new("/sdcard/123save.jpg")
+        del_this.delete
+        @touch_listener.undo_all
+        #RubotoActivity.launch(self)
       end
     end
   end
@@ -228,6 +246,8 @@ class RubotoActivity
   def self.launch(context)
     context.start_ruboto_activity("$activity_voyeur") do
       def on_create(bundle)
+        Debug.startMethodTracing("voyeurtrace")
+        Debug.startAllocCounting
         setTitle 'Voyeur'
         setContentView(
           linear_layout(:orientation => :vertical) do
